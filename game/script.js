c7f047c0a65db154d7ec2d918d6c869a384e1b6b
krolik2 <krolikmat@gmail.com>
2019-01-20 20:59:08 +0100

    added new point elements, fixed falling, added scoring and life

--- game/script.js
+++ game/script.js
@@ -5,6 +5,12 @@ var controls = {
     RIGHT: 39,
 };
 
+var scoreBoard = document.querySelector('.score');
+var score = 0;
+var scoreLife = document.querySelector('.life');
+var life = 3;
+
+
 function getActiveElement() {
     return document.querySelector('.active');
 }
@@ -13,50 +19,61 @@ function removeActiveClass() {
     getActiveElement().classList.remove('active');
 }
 
-function createBoard(rows, cells){
+function createBoard(rows, cells) {
     var table = '<table>';
-    for (var i = 0; i < rows; i++){
+    for (var i = 0; i < rows; i++) {
         table += '<tr>';
 
         for (var j = 0; j < cells; j++) {
             var isFirstCell = (j === 0 && i === rows - 1);
 
             if (isFirstCell) {
-                table += `<td x=${j} y=${i} class="cell active">1</td>`;
+                table += `<td x=${j} y=${i} class="cell active"></td>`;
             } else {
-                table += `<td x=${j} y=${i}  class="cell">1</td>`;
+                table += `<td x=${j} y=${i}  class="cell"></td>`;
             }
         }
 
         table += '</tr>';
-    } 
+    }
     table += '</table>';
     boardContainer.innerHTML = table;
+}
+
+function getElementBelow(tableCell) {
+
 }
 
 function generatePoints() {
     var cells = document.querySelectorAll('tr:first-child td:not(.points)');
     var randomIndex = Math.floor(Math.random() * cells.length);
-    cells[randomIndex].classList.add('points');
 
-    movePoints(randomIndex);
+    var rangeTable = [0.1, 0.3, 0.5, 1];
+    var randomNumber = Math.random();
+    var nextPoint = cells[randomIndex];
+    var range = rangeTable.find(function (el, index) {
+        return randomNumber < el;
+    });
+
+    switch (range) {
+        case 0.1:
+            return nextPoint.classList.add('points', 'mouse');
+        case 0.3:
+            return nextPoint.classList.add('points', 'milk');
+        case 0.5:
+            return nextPoint.classList.add('points', 'negative');
+        case 1:
+            return nextPoint.classList.add('points', 'drop');
+        default:
+            return nextPoint.classList.add('points', 'drop');
+    }
 }
 
-function movePoints(randomIndex) {
-    setInterval(() => {
-        var point = document.querySelector(`.points[x="${randomIndex}"]`);
-        var y = +point.getAttribute('y');
-        var x = +point.getAttribute('x');
-        var cellBelowCssSelector = `td[x="${x}"][y="${y + 1}"]`
-        var cell = document.querySelector(cellBelowCssSelector);
-
-        cell.classList.add('points');
-
-        var currentCellCssSelector = `td[x="${x}"][y="${y}"]`
-        var previous = document.querySelector(currentCellCssSelector);
-        previous.classList.remove('points');
-       
-    }, 500);
+function detectCollisions() {
+    var node = document.querySelector('td.active.points')
+    if (node) {
+        node.classList.remove('points')
+    }
 }
 
 
@@ -81,21 +98,64 @@ function moveLeft() {
 }
 
 function onControlChange(event) {
-    switch(event.keyCode) {
+    switch (event.keyCode) {
         case controls.LEFT:
             moveLeft();
             break;
         case controls.RIGHT:
             moveRight();
-        break;
-        
+            break;
+
     }
+    detectCollisions()
 }
 
 
 createBoard(10, 10);
-setInterval(generatePoints, 1000);
-movePoints();
+setInterval(generatePoints, 1501);
+setInterval(() => {
+    var points = document.querySelectorAll(`.points`);
+    points.forEach(point => {
+        var y = +point.getAttribute('y');
+        var x = +point.getAttribute('x');
+        var cellBelowCssSelector = `td[x="${x}"][y="${y + 1}"]`
+        var cell = document.querySelector(cellBelowCssSelector);
+
+        var kindOfPoint = ['negative', 'milk', 'mouse', 'drop'].find(function (el) {
+            return point.className.includes(el)
+        })
+
+        if (cell) {
+            isCellActive = cell.classList.contains('active');
+            switch (kindOfPoint) {
+                case 'mouse':
+                    isCellActive ? score += 3 : null;
+                    cell.classList.add('points', 'mouse');
+                    break;
+                case 'milk':
+                    isCellActive ? score += 2 : null;
+                    cell.classList.add('points', 'milk');
+                    break;
+                case 'drop':
+                    isCellActive ? score += 1 : null;
+                    cell.classList.add('points', 'drop');
+                    break;
+                case 'negative':
+                    isCellActive ? life -= 1 : null;
+                    cell.classList.add('points', 'negative');
+                    break;
+            }
+
+
+        } else {
+            life -= 1;
+        }
+        scoreBoard.innerText = score;
+        scoreLife.innerText = life;
+        point.classList.remove('points', 'milk', 'mouse', 'drop', 'negative');
+    })
+    detectCollisions()
+}, 500);
 document.addEventListener('keyup', onControlChange)
 
 /*************************************** */
