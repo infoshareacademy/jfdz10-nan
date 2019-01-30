9f9b184f6a37d04e3d6cca1266a75223195b1f10
Magda Skopczynska <madstar@interia.pl>
2019-01-30 21:40:02 +0100

    new file start_window

---
+++ game/start_window.js
@@ -0,0 +1,4 @@
+var body = document.querySelector('body');
+body.style.backgroundColor = `url(game_icons/board-game-bottom.png) no-repea`';
+
+var startWindow = document.querySelector('start__window');
