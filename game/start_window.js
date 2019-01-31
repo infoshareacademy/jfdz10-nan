1caa152ea340743c80280b4bd58fea2bfc81ff16
Magda Skopczynska <madstar@interia.pl>
2019-01-31 20:38:25 +0100

    buliding start site

--- game/start_window.js
+++ game/start_window.js
@@ -1,4 +1,13 @@
 var body = document.querySelector('body');
-body.style.backgroundColor = `url(game_icons/board-game-bottom.png) no-repea`';
+//body.style.backgroundColor = `url(game_icons/board-game-bottom.png) no-repeat`;
+var startWindow = document.querySelector('#start__board');
+var scores = document.querySelector('#scores');
+var menu = document.querySelector('.menu');
+var boardContainer = document.querySelector('#board__container');
 
-var startWindow = document.querySelector('start__window');
+menu.remove();
+scores.remove();
+boardContainer.remove();
+
+body.style.backgroundColor = `red`;
+
