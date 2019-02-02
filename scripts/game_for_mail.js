1a285bfc1c5f3e2ecf6b0ef3bf4aeb56dabb914c
Mateusz Krolikowski <krolikmat@gmail.com>
2019-02-02 17:38:17 +0100

    added: window size, refactored code, works on firefox

--- scripts/game_for_mail.js
+++
@@ -1,7 +0,0 @@
-const email = document.getElementById("email-form");
-email.addEventListener("submit", openGameWindow);
-
-function openGameWindow() {
-    window.open("http://127.0.0.1:5500/game/index.html", "_blank","height=500px width=300px " + //rozmiar okna do ustalenia
-    "resizable=no scrollbars=no menubar=no location=no status=no");
-}
