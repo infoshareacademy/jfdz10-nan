ce3017b8d98c787becf9afa18b55785fab55959c
bbakiewicz <bbakiewicz@gmail.com>
2019-02-05 10:52:36 +0100

    Addes styles to score table. Added title to score table. Only 10 highest scores will display.

--- game/player_results.js
+++ game/player_results.js
@@ -2,10 +2,20 @@ var scoresContainer = document.querySelector('#scores');
 var LS_SCORES_KEY = 'scores';
 
 function generateScoresAsNodeList() {
+    scoresContainer.classList.add('final__scores');
+    var finalScoreTitle = document.createElement('p');
+    scoresContainer.appendChild(finalScoreTitle);
+    finalScoreTitle.textContent = "Tablica wyników:";
+    finalScoreTitle.classList.add('final__scores--title');
+    var finalScoreList = document.createElement('div');
+    finalScoreList.classList.add('final__scores--list');
+    scoresContainer.appendChild(finalScoreList);
     const items = sortScoresASC(getScores())
-        .map((score, i) => `<li>${i + 1}. ${score.name} : ${score.points}</li>`).join('');
+        .map((score, i) => `<li>${i + 1}. ${score.name} : ${score.points}</li>`)
+        .filter((person, i) => i < 10).join('');
 
-    scoresContainer.innerHTML = `<ul>${items}</ul>`
+    finalScoreList.innerHTML = `<ul>${items}</ul>`;
+    finalScoreList.style.listStyle = 'none';
 }
 
 function saveScore(score) {
