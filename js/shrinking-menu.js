3e669c461cfacef2f9e9f56d594dcd02ace87c8f
Mateusz Krolikowski <krolikmat@gmail.com>
2019-02-08 22:34:16 +0100

    my approach

--- js/shrinking-menu.js
+++ js/shrinking-menu.js
@@ -1,8 +1,8 @@
-$(document).on("scroll", function shrinkingMenu() {
+$(window).on("scroll", function() {
 
-	if($(document).scrollTop()>100) {
-		$("nav").removeClass("menu").addClass("menu__small");
-	} else {
-		$("nav").removeClass("menu__small").addClass("menu");
-	}
-});
+	    if( $(this).width() > 768 && $(this).scrollTop()>=100 ) {
+	        $("nav").removeClass("menu").addClass("menu__small");
+	    }else {
+	        $("nav").removeClass("menu__small").addClass("menu");
+	    }
+	});
