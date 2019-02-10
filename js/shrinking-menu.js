42e01a27c2d23a57ea07786c6924df1d383c26a4
bbakiewicz <bbakiewicz@gmail.com>
2019-02-10 10:50:17 +0100

    menu shrinks only on desktops

--- js/shrinking-menu.js
+++ js/shrinking-menu.js
@@ -1,6 +1,6 @@
-$(document).on("scroll", function shrinkingMenu() {
+$(window).on("scroll", function shrinkingMenu() {
 
-	if($(document).scrollTop()>100) {
+	if($(window).scrollTop()>100 && $(window).width() > 768) {
 		$("nav").removeClass("menu").addClass("menu__small");
 	} else {
 		$("nav").removeClass("menu__small").addClass("menu");
