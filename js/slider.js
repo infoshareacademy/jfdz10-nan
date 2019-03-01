8325570c2072d08741faf9fd3288eb70127ee1a7
bbakiewicz <bbakiewicz@gmail.com>
2019-03-01 22:32:57 +0100

    cleaning function and variable names

--- js/slider.js
+++ js/slider.js
@@ -1,87 +1,80 @@
 window.onload = nextSlide;
 
-var nextButton = document.querySelector('.slider-button-next')
+var nextButton = document.querySelector(".slider-button-next");
 nextButton.addEventListener("click", nextSlide);
 
-var prevButton = document.querySelector('.slider-button-prev')
+var prevButton = document.querySelector(".slider-button-prev");
 prevButton.addEventListener("click", prevSlide);
 
-var dots = document.querySelectorAll('.slider-dots-element')
+var dots = document.querySelectorAll(".slider-dots-element");
 //dots.addEventListener("click", selectActiveSlide);
 
 var thisAd = 0;
 var slides = [
-    {
-        name: "slide1",
-        url: "url(../fotos/cat_background.jpg)"
-    },
-    {
-        name: "slide2",
-        url: "url(../fotos/cat_background2.jpg)"
-    },
-    {
-        name: "slide3",
-        url: "url(../fotos/cat_background3.jpg)"
-    },
+  {
+    name: "slide1",
+    url: "url(../fotos/cat_background.jpg)"
+  },
+  {
+    name: "slide2",
+    url: "url(../fotos/cat_background2.jpg)"
+  },
+  {
+    name: "slide3",
+    url: "url(../fotos/cat_background3.jpg)"
+  }
 ];
 
-const activeSlide = document.querySelector('.slider-dots-element-active');
+const activeSlide = document.querySelector(".slider-dots-element-active");
 
-var rotate =                   
-    setInterval(nextSlide, 7000);
+var rotate = setInterval(nextSlide, 7000);
 
 function setActiveSlide() {
-    dots.item().classList.add('slider-dots-element-active')
+  dots.item().classList.add("slider-dots-element-active");
 }
 
 function displayActiveDot() {
-    dots[thisAd].classList.add('slider-dots-element-active')
-    }
-    
-function removePrevDot() {
-    var prevDot = dots[thisAd].previousElementSibling;
-    if(prevDot === null) {
-        return dots[slides.length -1].classList.remove('slider-dots-element-active');
-    }
-    prevDot.classList.remove('slider-dots-element-active');
+  dots[thisAd].classList.add("slider-dots-element-active");
+}
+
+function removeLeftDot() {
+  var leftDot = dots[thisAd].previousElementSibling;
+  if (leftDot === null) {
+    return dots[slides.length - 1].classList.remove(
+      "slider-dots-element-active"
+    );
+  }
+  leftDot.classList.remove("slider-dots-element-active");
 }
 
 function removeRightDot() {
-    var rightDot = dots[thisAd].nextElementSibling;
-    if(rightDot === null) {
-        return dots[0].classList.remove('slider-dots-element-active');
-    }
-    rightDot.classList.remove('slider-dots-element-active');
+  var rightDot = dots[thisAd].nextElementSibling;
+  if (rightDot === null) {
+    return dots[0].classList.remove("slider-dots-element-active");
+  }
+  rightDot.classList.remove("slider-dots-element-active");
 }
 
-
-
 function nextSlide() {
-    thisAd++;
-    if(thisAd === slides.length) {
-      thisAd = 0;
-    }
-    var image = slides[thisAd];
-    document.getElementById("hero").style.backgroundImage = image.url; 
-    document.getElementById("hero").style.transition = "1s";
-    displayActiveDot();
-    removePrevDot();
-
+  thisAd++;
+  if (thisAd === slides.length) {
+    thisAd = 0;
+  }
+  var image = slides[thisAd];
+  document.getElementById("hero").style.backgroundImage = image.url;
+  document.getElementById("hero").style.transition = "1s";
+  displayActiveDot();
+  removeLeftDot();
 }
 
 function prevSlide() {
-    thisAd--;
-    if(thisAd < 0) {
-        thisAd = slides.length - 1;
-    }
-    var image = slides[thisAd];
-    document.getElementById("hero").style.backgroundImage = image.url; 
-    document.getElementById("hero").style.transition = "1s";
-    displayActiveDot();  
-    removeRightDot();
+  thisAd--;
+  if (thisAd < 0) {
+    thisAd = slides.length - 1;
+  }
+  var image = slides[thisAd];
+  document.getElementById("hero").style.backgroundImage = image.url;
+  document.getElementById("hero").style.transition = "1s";
+  displayActiveDot();
+  removeRightDot();
 }
-
-
-
-
-
