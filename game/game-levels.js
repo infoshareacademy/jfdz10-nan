0d5210afba242871e5ec7b374144de4cf390dce3
bbakiewicz <bbakiewicz@gmail.com>
2019-03-08 22:27:05 +0100

    level board appears when player reach 20 points

--- game/game-levels.js
+++ game/game-levels.js
@@ -4,6 +4,22 @@ function nextLevel() {
         clearInterval(fallingElementsIntervalId);
         removeActiveClass();
         removeAllPoints();
+        showLevelBoard();
     }
     
+}
+
+function showLevelBoard() {
+        var gameResultTitle = document.createElement('p');
+        var gameResult = document.createElement('p');
+        
+        finalScoreContainer.classList.add('game__result');
+    
+        finalScoreContainer.appendChild(gameResultTitle);
+        gameResultTitle.textContent = "Poziom";
+        gameResultTitle.classList.add('game__result--title')
+        
+        finalScoreContainer.appendChild(gameResult);
+        gameResult.textContent = 2;
+        gameResult.classList.add('game__result--score');
 }
